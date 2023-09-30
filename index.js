29c1b7e7b4213218b93b08f0f8291bcae0ca297e
Sundeep <sundeep.kaler@gmail.com>
2023-09-30 07:15:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(records) {
+    let foundRecord = records.find(record => record.result === "W");
+    if(foundRecord === undefined){
+        return undefined;
+    }
+    else { 
+        return foundRecord.year;
+    }
+}
